053f44b8b47b8fc60ab51e3c09e46e63cdc99b40
Anastasia Shlyk <anastasiashlyk@MacBook-Air-de-Anastasia.local>
2025-10-21 21:54:13 +0200

    mod

--- coleccionesInterdependientes.hpp
+++ coleccionesInterdependientes.hpp
@@ -83,40 +83,43 @@ template<typename ident, typename val> void avanza(colecInterdep<ident, val>& c)
 
 // DECLARACION DEL TAD GENERICO colecInterdep
 
-template<typename T>
-struct cola{
-    friend void crear(cola<T>& c);
-    friend void encolar(cola<T>& c, const T& e);
-    friend bool esVacía?(const cola<T>& c);
-    friend void primero(cola<T>& c,  T& p);
-    friend void desencolar(cola<T>& c);
-    friend int longitud(const cola<T>& c);
-    //COMPLETAR OPERACIONES ITERADOR
-    //....
-    friend void iniciarIterador (cola<T>& c);
-    friend bool existeSiguiente (const cola<T>& c);
-    friend bool siguiente (cola<T>& c);
-    friend bool avanza (cola<T>& c, T& c);
-    
+template<typename ident, typename val>
+struct colecInterdep{
+  friend template<typename ident, typename val> void crear(colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> bool esVacia(const colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> bool existe(const colecInterdep<ident, val>& c, const ident& id);
+  friend template<typename ident, typename val> unsigned int tamanyo(const colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> bool existeDependiente(const colecInterdep<ident, val>& c, const ident& id);
+  friend template<typename ident, typename val> bool existeIndependiente(const colecInterdep<ident, val>& c, const ident& id);
+  friend template<typename ident, typename val> bool anadirIndependiente(colecInterdep<ident, val>& c, const ident& id, const val& v);
+  friend template<typename ident, typename val> bool anadirDependiente(colecInterdep<ident, val>& c, const ident& id, const val& v, const ident& super);
+  friend template<typename ident, typename val> bool hacerDependiente(colecInterdep<ident, val>& c, const ident& id, const ident& super);
+  friend template<typename ident, typename val> bool hacerIndependiente(colecInterdep<ident, val>& c, const ident& id);
+  friend template<typename ident, typename val> bool actualizarVal(colecInterdep<ident, val>& c, const ident& id, const val& nuevo);
+  friend template<typename ident, typename val> val obtenerVal(const colecInterdep<ident, val>& c, const ident& id);
+  friend template<typename ident, typename val> ident obtenerSupervisor(const colecInterdep<ident, val>& c, const ident& id);
+  friend template<typename ident, typename val> unsigned int obtenerNumDependientes(const colecInterdep<ident, val>& c, const ident& id);
+  friend template<typename ident, typename val> bool borrar(colecInterdep<ident, val>& c, const ident& id);
+  friend template<typename ident, typename val> void iniciarIterador( colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> bool existeSiguiente(const colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> ident siguienteIdent(const colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> val siguienteVal(const colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> bool siguienteDependiente(const colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> ident siguienteSuperior(const colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> unsigned int siguienteNumDependientes(const colecInterdep<ident, val>& c);
+  friend template<typename ident, typename val> void avanza(colecInterdep<ident, val>& c);
+
   private: 
-     //campos
     struct Nodo {
-      //ident que se usaremos para saber donde posicionar el nodo
         ident id;
-      //valor del nodo
         val valor;
-      //Puntero que apuntara a su 
-        Nodo* sup;
-      //Puntero que apunta al siguiente integrante
+        ident identSup;
+        unsigned int numDepend = 0;
         Nodo* siguiente;
-      //Cantidad de nodos dependientes a este
-        unsigned int NumDepend;
+      
     };
-    //Puntero que apunta al primer integrante de la colección
       Nodo* primElmt;
-    //Entero con la cantidad total de elementos de la colección
       unsigned int numElem;
-    //Puntero que sirve como el cursor del iterador
       Nodo* iter;
 };
 
@@ -125,13 +128,7 @@ struct cola{
 
 //
 template<typename ident, typename val> void crear(colecInterdep<ident, val>& c){
-  //Creamos un el nodo centinela anteriormente explicado
-   //typename colecInterdep<ident, val>::Nodo* aux = new typename  colecInterdep<ident, val>::Nodo;
-    //Este no apuntara a nada puesto que todavia no hay datos
-    aux->sig = nullptr;
-  //Apuntamos al nodo centinela como primer elemento "ficticio"
-    c.primElmt=aux;
-  //Definimos la variable de numero de elementos de la colección a 0
+    c.primElmt = nullptr;
     c.numElem=0;
 }
 
@@ -150,28 +147,49 @@ template<typename ident, typename val> bool esVacia(const colecInterdep<ident, v
 
 //(Divide y venceras en lista ordenada no se debe de poder, pararemos cuando el id actual sea mayor al que estamos buscando o si ya no quedan elementos que explorar. Intentar jugar con como recorrer el orden de la lista o busquedas bidireccionales para reducir coste en tiempo no tiene sentido al no ser ni doble enlazada.)
 template<typename ident, typename val> bool existe(const colecInterdep<ident, val>& c, const ident& id){
-  // Primero de todo comprovamos que no sea vacia, para en caso de serlo salir con el minimo coste, puesto que sera seguro que no este el dato.
-    if (esVacia) {return false}
-  //Generamos un puntero auxiliar para recorrer el lista
-    typename colecInterdep<ident, val>::Nodo* aux = new typename       colecInterdep<ident, val>::Nodo;
-    //Y lo dejamos apuntando al primer elemento para empezar la busqueda
-    aux = c.primElmt;
-  //Nos ponemos en posicion para apuntar a los elementos y entrar en el bucle de busqueda
-    aux = aux->sig;
-  //Empezamos el bucle
-    while (aux.sig!=nullptr && aux.id>)
-    
+  if(esVacia(c)){
+    return false;
+  }else{
+    colecInterdep<ident, val> ::Nodo* aux = c.primElmt;
+    while(aux != nullptr && aux->id <= id){
+      if(aux->id == id){
+        return true;
+      }
+      aux = aux->siguiente;
+    }
+    return false;
+  }
 }
 
 //
-template<typename ident, typename val> bool existeDependiente(const colecInterdep<ident, val>& c, const ident& id);
+template<typename ident, typename val> bool existeDependiente(const colecInterdep<ident, val>& c, const ident& id){
+  if(esVacia(c)){
+    return false;
+  }else{
+    colecInterdep<ident, val> ::Nodo* aux = c.primElmt;
+    while(aux != nullptr && aux->id <= id){
+      if(aux->id == id && existe(c, aux->identSup)){
+        return true;
+      }
+      aux = aux->siguiente;
+    }
+    return false;
+  }
+}
 
 //
 template<typename ident, typename val> bool existeIndependiente(const colecInterdep<ident, val>& c, const ident& id);
 
 
 //
-template<typename ident, typename val> bool anadirIndependiente(colecInterdep<ident, val>& c, const ident& id, const val& v);
+template<typename ident, typename val> void anadirIndependiente(colecInterdep<ident, val>& c, const ident& id, const val& v){
+  if(existe(c, id)){
+    return false; // ya existe, no lo hemos añadido, habrá que modificar el valor
+  } else{
+    c.primElmt = new typename colecInterdep<ident, val>::Nodo;
+    primElmt
+  }
+}
 
 //
 template<typename ident, typename val> bool anadirDependiente(colecInterdep<ident, val>& c, const ident& id, const val& v, const ident& super);
